13c25dfdcfa4d98c7b5495e5f44de30432b47380
JokerCM <googlcfma@gmail.com>
2019-03-08 11:05:09 +0800

    Update model.py
    Sorry, is there a mistake in line45 "output = self.dropout_layer(ht[-1])"?

--- model.py
+++ model.py
@@ -42,7 +42,7 @@ class LSTMClassifier(nn.Module):
 		# ht is the last hidden state of the sequences
 		# ht = (1 x batch_size x hidden_dim)
 		# ht[-1] = (batch_size x hidden_dim)
-		output = self.dropout_layer(ht[-1])
+		output = self.dropout_layer(outputs[-1])
 		output = self.hidden2out(output)
 		output = self.softmax(output)
 
